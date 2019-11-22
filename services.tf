efad36bf2fd66a1d30792692b6dce39fe9c42eb8
salmant <salman.taherizadeh@gmail.com>
2019-11-22 16:53:44 +0100

    Update services.tf

--- services.tf
+++ services.tf
@@ -41,6 +41,7 @@ resource "aws_ecs_service" "web-service" {
     cluster = "${aws_ecs_cluster.main.id}"
     task_definition = "${aws_ecs_task_definition.web-service.arn}"
     iam_role = "${aws_iam_role.ecs_service_role.arn}"
+    # (Optional) The number of instances of the task definition to place and keep running. Create service with 2 instances to start.
     desired_count = 2
     depends_on = ["aws_iam_role_policy.ecs_service_role_policy"]
 
